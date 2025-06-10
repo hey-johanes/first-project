29d348c2785dc6156bd98cae96952bf2f47997c3
johnkristiadi <johnkristiadi@gmail.com>
2025-06-10 18:56:15 +0700

    add product feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import './App.css';
 import ProductCard from './Component/ProductCard';
+import AddProduct from './Component/AddProduct';
 import { Products } from './data/Products';
 
 const App = () => {
+  const [show, setShow] = useState(false);
+  const [product, setProduct] = useState(Products);
+  const showAddProduct = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  const handleAddProduct = (newData) => {
+    setProduct((product) => [...product, newData]);
+    setShow(false);
+  };
+
   return (
-    <div className="cards">
-      {Products.map((item) => (
-        <ProductCard
-          key={item.id}
-          nama={item.nama}
-          deskripsi={item.deskripsi}
-          url={item.imageURL}
-        />
-      ))}
+    <div>
+      <div>
+        <div className="app-title">List Mobil</div>
+        {show ? (
+          <>
+            <AddProduct handleAddProduct={handleAddProduct}></AddProduct>
+            <button className="button-add-product" onClick={showAddProduct}>
+              Close Add Product
+            </button>
+          </>
+        ) : (
+          <button className="button-add-product" onClick={showAddProduct}>
+            Add Product
+          </button>
+        )}
+      </div>
+      <div className="cards">
+        {product.map((item) => (
+          <ProductCard
+            key={item.id}
+            nama={item.nama}
+            deskripsi={item.deskripsi}
+            url={item.imageURL}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/Component/ProductCard.jsx
+++ src/Component/ProductCard.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
-import { useState } from 'react';
+import { React, useState } from 'react';
+import { Trash2 } from 'lucide-react';
 
 const ProductCard = ({ nama, deskripsi, url }) => {
   const [amount, setAmount] = useState(0);
   const addAmount = () => {
-    setAmount(amount + 1);
+    setAmount((amount) => amount + 1);
   };
   const minAmount = () => {
-    setAmount(amount - 1);
+    setAmount((amount) => amount - 1);
+  };
+  const defaulValue = () => {
+    setAmount(0);
   };
   return (
     <div className="card">
-      <img
-        style={{
-          width: '100%',
-          height: '200px',
-          borderRadius: '10px 10px 0px 0px',
-        }}
-        alt="gambar"
-        src={url}
-      ></img>
+      <img className="card-img" alt="gambar" src={url}></img>
       <div className="container">
         <h4>{nama}</h4>
         <p>{deskripsi}</p>
-        <div>
-          <button className="add-toggle" onClick={addAmount}>
-            +
-          </button>
-          <button className="add-toggle" onClick={minAmount}>
-            -
-          </button>
-          <p>{amount}</p>
+        <div className={`card-keranjang`}>
+          {amount === 0 ? (
+            <button
+              className={`card-keranjang ${amount > 0 ? 'jumlah-product' : 'show-keranjang'}`}
+              onClick={addAmount}
+            >
+              Tambah Keranjang
+            </button>
+          ) : (
+            <>
+              <button className="button" onClick={minAmount}>
+                -
+              </button>
+              <p>{amount}</p>
+              <button className="button" onClick={addAmount}>
+                +
+              </button>
+              <Trash2 onClick={defaulValue}></Trash2>
+            </>
+          )}
         </div>
       </div>
     </div>
